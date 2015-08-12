05a2e87bf7bf0e589bf4266c5b3c008868f2c51d
chinakite <chinakite.zhang@gmail.com>
2015-08-12 10:52:44 +0800

    修改setParameter方法中Float与Double转换bug

--- src/com/ideamoment/ideajdbc/executor/SqlExecutor.java
+++ src/com/ideamoment/ideajdbc/executor/SqlExecutor.java
@@ -126,7 +126,7 @@ public class SqlExecutor {
 						pstmt.setFloat(i, (Float)paramValue);
 						break;
 					case DOUBLE:
-						pstmt.setDouble(i, (Float)paramValue);
+						pstmt.setDouble(i, (Double)paramValue);
 						break;
 					case DECIMAL:
 						pstmt.setBigDecimal(i, (BigDecimal)paramValue);
